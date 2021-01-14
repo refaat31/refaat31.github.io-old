a8a1034a8160d9041af4cb49f2b2a278f157e361
refaat31 <refaatalamgir@iut-dhaka.edu>
2021-01-14 22:44:43 +0600

    enhance burger menu

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const navSlide = () => {
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
-  });
 
-  // animate links
-  navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    // animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
+      }
+    });
+
+    // burger animation
+    burger.classList.toggle('toggle');
   });
 };
 
