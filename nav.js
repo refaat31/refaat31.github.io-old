de0dc2cd3139453c2bd266fec388528fd544bcc4
refaat31 <refaatalamgir@iut-dhaka.edu>
2021-03-02 16:20:05 +0600

    nav.js added

---
+++ nav.js
@@ -0,0 +1,34 @@
+document.write(`
+  <nav>
+    <div class='logo'>
+      <h4>shreddedcheese</h4>
+    </div>
+    <ul class='nav-links'>
+      <li>
+        <a href='./index.html' class='underline-effect'>
+          Home
+        </a>
+      </li>
+      <li>
+        <a href='./about.html' class='underline-effect'>
+          About
+        </a>
+      </li>
+      <li>
+        <a href='#' class='underline-effect'>
+          Projects
+        </a>
+      </li>
+      <li>
+        <a href='#' class='underline-effect'>
+          Contact
+        </a>
+      </li>
+    </ul>
+    <div class='burger'>
+      <div class='line1'></div>
+      <div class='line2'></div>
+      <div class='line3'></div>
+    </div>
+  </nav>
+`);
