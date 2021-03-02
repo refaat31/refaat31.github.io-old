da8dd1dd7863bc1560666a3313233743fbcbdf07
refaat31 <refaatalamgir@iut-dhaka.edu>
2021-03-02 18:11:00 +0600

    hardcoded current nav highlight

--- nav.js
+++ nav.js
@@ -1,34 +1,73 @@
-document.write(`
-  <nav>
-    <div class='logo'>
-      <h4>shreddedcheese</h4>
-    </div>
-    <ul class='nav-links'>
-      <li>
-        <a href='./index.html' class='underline-effect'>
-          <div class='current'>Home</div>
-        </a>
-      </li>
-      <li>
-        <a href='./about.html' class='underline-effect'>
-          <div>About</div>
-        </a>
-      </li>
-      <li>
-        <a href='#' class='underline-effect'>
-          <div>Projects</div>
-        </a>
-      </li>
-      <li>
-        <a href='#' class='underline-effect'>
-          <div>Contact</div>
-        </a>
-      </li>
-    </ul>
-    <div class='burger'>
-      <div class='line1'></div>
-      <div class='line2'></div>
-      <div class='line3'></div>
-    </div>
-  </nav>
-`);
+const url = document.URL;
+
+if (url.search('index.html') != -1) {
+  document.write(`
+    <nav>
+      <div class='logo'>
+        <h4>shreddedcheese</h4>
+      </div>
+      <ul class='nav-links'>
+        <li>
+          <a href='./index.html' class='underline-effect'>
+            <div class='current'>Home</div>
+          </a>
+        </li>
+        <li>
+          <a href='./about.html' class='underline-effect'>
+            <div>About</div>
+          </a>
+        </li>
+        <li>
+          <a href='#' class='underline-effect'>
+            <div>Projects</div>
+          </a>
+        </li>
+        <li>
+          <a href='#' class='underline-effect'>
+            <div>Contact</div>
+          </a>
+        </li>
+      </ul>
+      <div class='burger'>
+        <div class='line1'></div>
+        <div class='line2'></div>
+        <div class='line3'></div>
+      </div>
+    </nav>
+  `);
+} else if (url.search('about.html') != -1) {
+  document.write(`
+    <nav>
+      <div class='logo'>
+        <h4>shreddedcheese</h4>
+      </div>
+      <ul class='nav-links'>
+        <li>
+          <a href='./index.html' class='underline-effect'>
+            <div>Home</div>
+          </a>
+        </li>
+        <li>
+          <a href='./about.html' class='underline-effect'>
+            <div class='current'>About</div>
+          </a>
+        </li>
+        <li>
+          <a href='#' class='underline-effect'>
+            <div>Projects</div>
+          </a>
+        </li>
+        <li>
+          <a href='#' class='underline-effect'>
+            <div>Contact</div>
+          </a>
+        </li>
+      </ul>
+      <div class='burger'>
+        <div class='line1'></div>
+        <div class='line2'></div>
+        <div class='line3'></div>
+      </div>
+    </nav>
+  `);
+}
